e67c4e9c13ff39eb9b7e51b17eac815ecefba18d
Carter Simonson <cartersimonson01@gmail.com>
2022-06-26 21:32:51 -0500

    Modify project margins

--- components/Projects/index.js
+++ components/Projects/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { projectImage } from './styles.module.scss'
 
-function Project({ title, img, alt, children }) {
+function Project({ title, img, alt, children, className }) {
   return (
-    <div className='margin-bottom-xl'>
+    <div className={className}>
         <img className={projectImage} src={img} alt={alt} />
         <h3>{ title }</h3>
         { children }
@@ -13,13 +13,14 @@ function Project({ title, img, alt, children }) {
 
 export default function Projects() {
   return (
-    <>
+    <div className='margin-bottom-xl'>
       <h2 className='section-header'>PROJECTS</h2>
 
       <Project
         title='Kenshi Interactive Map'
         img='/assets/projects/kenshimap.webp'
         alt='Kenshi Map Screenshot'
+        className='margin-bottom-lg'
       >
         <p>
           An interactive map application for the video game Kenshi by Lo-Fi Games. This originated in early 2019 as a weekend project which I built using
@@ -36,10 +37,10 @@ export default function Projects() {
         <p>
           A basic URL shortener implementation written with Flask, SQLite, and vanilla HTML/CSS/JS. The service is configured with Github actions to automatically build and deploy
           to a DigitalOcean Droplet on each commit. My goal with this project was to refresh my knowledge on Python, SQL, Docker, Nginx, and Github Actions.
-          A live demo exists at <a href='https://shorten.minidev.io' target='_blank' rel='noopener'>shorten.minidev.io</a>.
+          A live demo can be found at <a href='https://shorten.minidev.io' target='_blank' rel='noopener'>shorten.minidev.io</a>.
           The source code can be found <a href='https://github.com/CarterSimonson/miniurl' target='_blank' rel='noopener'>here</a>.
         </p>
       </Project>
-    </>
+    </div>
   );
 }
